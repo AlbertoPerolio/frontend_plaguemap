6ff08e7d6e69b61daf5da0c9baa4d1da89355f3b
AlbertoPerolio <albertoperolio0319@gmail.com>
2025-10-14 23:22:48 -0300

    .

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,15 +6,15 @@ export const loginRequest = async (user) => {
   // Login normal (envía credenciales)
   const res = await axios.post("/auth/login", user, { withCredentials: true });
 
-  // ⚡ Esperar un poco antes de verificar el token (para evitar “Token requerido”)
+  // Esperar un poco antes de verificar el token (para evitar “Token requerido”)
   setTimeout(async () => {
     try {
       const verifyRes = await axios.get("/auth/verify", {
         withCredentials: true,
       });
-      console.log("✅ Token verificado:", verifyRes.data);
+      console.log("Token verificado:");
     } catch (err) {
-      console.warn("⚠️ Error al verificar token tras login:", err);
+      console.warn("Error al verificar token tras login:", err);
     }
   }, 400);
 
