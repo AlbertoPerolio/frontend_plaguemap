3237f8512ab4ab6a6fae72e2d659f9d887f2f68c
AlbertoPerolio <albertoperolio0319@gmail.com>
2026-04-07 08:33:41 -0300

    .

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -27,7 +27,9 @@ function RegisterPage() {
         <form onSubmit={onSubmit}>
           <h3 className="heading-register">Crear una cuenta</h3>
           <div className="register">
-            {<div className="text-exist">{registerErrors.body?.mensaje}</div>}
+            {registerErrors?.message && (
+              <div className="text-exist">{registerErrors.message}</div>
+            )}
             <div className="register-field">
               <label>Nombre:</label>
               <input
